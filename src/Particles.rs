ec570d5efe555fb73bd3ec30b9da3ce60fcaadc5
Kyle Kovalchick <kovalchickzz@gmail.com>
2023-05-22 17:34:44 -0500

    Updated particle system

--- src/Particles.rs
+++ src/Particles.rs
@@ -7,11 +7,12 @@ use crate::ship::interactables_controllers::Valve;
 
 pub fn update_particles(
     valve_query: Query<(&Valve)>,
-    mut particles: Query<&mut ParticleEffect>,
+    mut particles: Query<(&mut CompiledParticleEffect,&mut EffectSpawner)>,
 ) {
     if let Some(valve) = valve_query.iter().find(|valve| valve.identifier == 0) {
-        if let Ok(mut spawner) = particles.get_single_mut() {
-            //spawner.maybe_spawner().unwrap().set_active(valve.current_value > 1.);
+        if let Ok((mut cpe, mut spawner)) = particles.get_single_mut() {
+            cpe.set_property("my_value", (valve.current_value/2.0).into());
+            //spawner.set_active(valve.current_value == 0.0);
         }
     }
 }
@@ -29,30 +30,37 @@ pub fn setup_particles(
     gradient.add_key(0.5, Vec4::from_array(Color::WHITE.as_rgba_f32())); // White
     gradient.add_key(1.0, Vec4::ZERO); // Transparent Black
 
+
+    let spawner = Spawner::rate(30.0.into());
+
     let effect = effects.add(EffectAsset {
         name: "Water".to_string(),
         capacity: 30000,
-        spawner: Spawner::rate(100.0.into()),
+        spawner,
         ..default()
-    }.init(InitPositionCone3dModifier {
-        height: 5.0,
-        base_radius: 3.0,
+    }.with_property("my_value", graph::Value::Float(5.0))
+        .init(InitAttributeModifier{
+            attribute: Attribute::LIFETIME,
+            value: "my_value".into(),
+        })
+        .init(InitPositionCone3dModifier {
+        height: 20.1,
+        base_radius: 10.1,
         top_radius: 1.0,
-        dimension: ShapeDimension::Surface,
+        dimension: ShapeDimension::Volume,
     }
     ).init(InitVelocitySphereModifier {
         center: Vec3::ZERO,
-        speed: 5.0_f32.into(),
-    }
-    ).init(InitLifetimeModifier { lifetime: 10_f32.into() })
+        speed: (-10.0_f32).into(),
+    })
      .update(AccelModifier::constant(Vec3::new(0., -9.8, 0.)))
      .render(ColorOverLifetimeModifier { gradient})
         .render(BillboardModifier)
     );
 
     commands.spawn(ParticleEffectBundle {
-        effect: ParticleEffect::new(effect),
-        transform: Transform::from_translation(Vec3::new(0., 1., 0.)),
+        effect: ParticleEffect::new(effect).with_spawner(spawner),
+        transform: Transform::from_translation(Vec3::new(0., 10., 0.)),
         ..default()
     })
         .insert(Particle{});
